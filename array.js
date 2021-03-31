c2e21eedca363bf9bad7ca4f5797c09419fbabcc
TENCENT\v_haxzhuang <haxzhuang@tencent.com>
2021-03-31 15:11:50 +0800

    F 更新lodash array代码

--- array.js
+++ array.js
@@ -1,65 +1,75 @@
-_.chunk
-_.compact
-_.concat
-_.difference
-_.differenceBy
-_.differenceWith
-_.drop
-_.dropRight
-_.dropRightWhile
-_.dropWhile
-_.fill
-_.findIndex
-_.findLastIndex
-_.first - > head
-_.flatten
-_.flattenDeep
-_.flattenDepth
-_.fromPairs
-_.head
-_.indexOf
-_.initial
-_.intersection
-_.intersectionBy
-_.intersectionWith
-_.join
-_.last
-_.lastIndexOf
-_.nth
-_.pull
-_.pullAll
-_.pullAllBy
-_.pullAllWith
-_.pullAt
-_.remove
-_.reverse
-_.slice
-_.sortedIndex
-_.sortedIndexBy
-_.sortedIndexOf
-_.sortedLastIndex
-_.sortedLastIndexBy
-_.sortedLastIndexOf
-_.sortedUniq
-_.sortedUniqBy
-_.tail
-_.take
-_.takeRight
-_.takeRightWhile
-_.takeWhile
-_.union
-_.unionBy
-_.unionWith
-_.uniq
-_.uniqBy
-_.uniqWith
-_.unzip
-_.unzipWith
-_.without
-_.xor
-_.xorBy
-_.xorWith
-_.zip
-_.zipObject
-_.zipObjectDeep
-_.zipWith
+var _ = require('lodash');
+// _.chunk 数组分块
+// const newArr = _.chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)
+// console.log('newArr', newArr);
+
+// _.compact 过滤假值
+//  
+// _.concat 连接，不改变原数组
+// const arr = [1, 2, 3]
+// const arr2 = [4, 5, [6]]
+// const newArr = _.concat(arr, arr2)
+// console.log('arr', arr);
+// console.log('newArr', newArr);
+// _.difference 过滤值 
+// _.differenceBy
+// _.differenceWith
+// _.drop
+// _.dropRight
+// _.dropRightWhile
+// _.dropWhile
+// _.fill
+// _.findIndex
+// _.findLastIndex
+// _.first - > head
+// _.flatten
+// _.flattenDeep
+// _.flattenDepth
+// _.fromPairs
+// _.head
+// _.indexOf
+// _.initial
+// _.intersection
+// _.intersectionBy
+// _.intersectionWith
+// _.join
+// _.last
+// _.lastIndexOf
+// _.nth
+// _.pull
+// _.pullAll
+// _.pullAllBy
+// _.pullAllWith
+// _.pullAt
+// _.remove
+// _.reverse
+// _.slice
+// _.sortedIndex
+// _.sortedIndexBy
+// _.sortedIndexOf
+// _.sortedLastIndex
+// _.sortedLastIndexBy
+// _.sortedLastIndexOf
+// _.sortedUniq
+// _.sortedUniqBy
+// _.tail
+// _.take
+// _.takeRight
+// _.takeRightWhile
+// _.takeWhile
+// _.union
+// _.unionBy
+// _.unionWith
+// _.uniq
+// _.uniqBy
+// _.uniqWith
+// _.unzip
+// _.unzipWith
+// _.without
+// _.xor
+// _.xorBy
+// _.xorWith
+// _.zip
+// _.zipObject
+// _.zipObjectDeep
+// _.zipWith
